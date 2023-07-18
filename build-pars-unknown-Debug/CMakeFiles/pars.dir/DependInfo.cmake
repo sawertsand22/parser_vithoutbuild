cfa6bf3ad14140de055e942c5ad12fcc9f0f4e71
sawertsand22 <85335401+sawertsand22@users.noreply.github.com>
2023-07-18 04:41:43 -0700

    Add files via upload

---
+++ build-pars-unknown-Debug/CMakeFiles/pars.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sawert/qtprojects/pars/main.cpp" "/home/sawert/qtprojects/build-pars-unknown-Debug/CMakeFiles/pars.dir/main.cpp.o"
+  "/home/sawert/qtprojects/build-pars-unknown-Debug/pars_autogen/mocs_compilation.cpp" "/home/sawert/qtprojects/build-pars-unknown-Debug/CMakeFiles/pars.dir/pars_autogen/mocs_compilation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_PROGRAM_OPTIONS_DYN_LINK"
+  "QT_CORE_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/sawert/qtprojects/pars"
+  "pars_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
